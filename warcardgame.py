772a63eb6d847453d50b581a6964f67addadb766
LinuxMemory <93434579+LinuxMemory@users.noreply.github.com>
2022-10-20 18:50:31 +0700

    Card and Deck classes

---
+++ warcardgame.py
@@ -0,0 +1,35 @@
+import random
+
+
+suits = ["Spades","Clubs", "Arrows", "Diamonds"]
+
+values = {"Two":2,"Three":3, "Four":4, "Five":5, "Six":6, "Seven":7, "Eight":8, "Nine":9, "Ten":10, "Jack":11, "Queen":12, "King":13, "Ace":14}
+
+ranks = ['Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine', 'Ten', 'Jack', 'Queen', 'King', 'Ace']
+
+
+class Card():
+    def __init__(self, suits, rank):
+        self.suits = suits
+        self.rank = rank
+        self.value = values[rank]
+    
+    def __str__(self):
+        return self.rank + " of " + self.suits
+
+
+
+class Deck:
+    def __init__(self):
+        self.all_cards = []
+        for suit in suits:
+            for rank in ranks:
+                created_card = Card(suit, rank)
+                self.all_cards.append(created_card)
+    
+    def shuffle(self):
+        random.shuffle(self.all_cards)
+
+
+    def deal_one(self):
+        return self.all_cards.pop()
